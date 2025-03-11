ab844770af1f88f627942661aae79b3d69d004df
Natalia Nefedova <nntalik@gmail.com>
2025-03-11 20:09:10 +0100

    hero gradient all pages

--- src/pages/Tracker.tsx
+++ src/pages/Tracker.tsx
@@ -204,11 +204,11 @@ const Tracker = () => {
       <div className="sticky top-0 z-50">
         <NavBar />
         
-        <div className="bg-gradient-to-br from-primary to-primary-hover sticky top-0 z-50">
+        <div className="bg-gradient-to-br from-custom-green-1 to-custom-green-2 sticky top-0 z-50">
           <div className="max-w-[1200px] mx-auto px-4 py-4 md:py-6 text-center">
             <h1 className="text-3xl md:text-4xl font-bold text-gray-900 mb-2">Application Tracker</h1>
             <p className="text-gray-600 max-w-2xl mx-auto">
-              Track the status of your job applications in one place
+              Track the status of your job applications
             </p>
           </div>
         </div>
